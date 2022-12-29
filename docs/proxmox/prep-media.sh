0aefeeed50ee3ed5f627083c80f7db6d194142e7
codecio <matt@colecio.com>
2022-12-28 23:44:13 -0600

    fix: Typo in proxmox name in imaging script.

--- docs/proxmox/prep-media.sh
+++ docs/proxmox/prep-media.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# promox variables
-promox_ve_ver="7.3-1"
-promox_ve_url_domain="http://download.proxmox.com"
-promox_ve_url_path="/iso/"
-promox_ve_url_base="$promox_ve_url_domain$promox_ve_url_path"
-promox_ve_url_page="proxmox-ve_$promox_ve_ver.iso"
-promox_ve_url_iso_sha256sums="SHA256SUMS"
+# proxmox variables
+proxmox_ve_ver="7.3-1"
+proxmox_ve_url_domain="http://download.proxmox.com"
+proxmox_ve_url_path="/iso/"
+proxmox_ve_url_base="$proxmox_ve_url_domain$proxmox_ve_url_path"
+proxmox_ve_url_page="proxmox-ve_$proxmox_ve_ver.iso"
+proxmox_ve_url_iso_sha256sums="SHA256SUMS"
 
 # Pre-flight check argument is valid.
 disk_target=$1
@@ -28,17 +28,17 @@ function validate_url()
     return $?
 }
 
-if validate_url $promox_ve_url_base$promox_ve_url_page; then
+if validate_url $proxmox_ve_url_base$proxmox_ve_url_page; then
     # Get page and check if file already exists in current working directory.
-    wget --no-clobber $promox_ve_url_base$promox_ve_url_page
+    wget --no-clobber $proxmox_ve_url_base$proxmox_ve_url_page
     else
     # Print error message.
     command && echo "download error: validation failed." && exit 1
 fi
 
 # Extract and match iso version with sha256sum.
-extracted_iso_shasum=$(curl -s $promox_ve_url_base$promox_ve_url_iso_sha256sums |\
-grep "$promox_ve_url_page" |\
+extracted_iso_shasum=$(curl -s $proxmox_ve_url_base$proxmox_ve_url_iso_sha256sums |\
+grep "$proxmox_ve_url_page" |\
 cut -f 1 -d ' ')
 
 function verify_iso()
@@ -46,7 +46,7 @@ function verify_iso()
     echo "$1" "*$2" | shasum -a 256 --check
 }
 
-if verify_iso "$extracted_iso_shasum" "$promox_ve_url_page"; then
+if verify_iso "$extracted_iso_shasum" "$proxmox_ve_url_page"; then
     echo "shasum verified: OK"
     else
     echo "shasum verified: NO" && exit 1
@@ -58,7 +58,7 @@ read -r -n1 -p "You are about to erase /dev/$disk_target. Are you sure? [y/N] "
 echo
 case $REPLY in
     y | Y)
-    echo "Copy and convert /dev/$disk_target with $promox_ve_url_page "
+    echo "Copy and convert /dev/$disk_target with $proxmox_ve_url_page "
     ;;
     * )
     echo "Aborting..."
@@ -69,21 +69,21 @@ esac
 # Simple image file copy to USB flash drive.
 function create_usb()
 {
-    sudo dd bs=100M if=./$promox_ve_url_page of=/dev/"$1" status=progress
+    sudo dd bs=100M if=./$proxmox_ve_url_page of=/dev/"$1" status=progress
 }
 
 if create_usb "$disk_target"; then
     # USB imaged
-    echo "/dev/$disk_target imaged with $promox_ve_url_page"
+    echo "/dev/$disk_target imaged with $proxmox_ve_url_page"
     else
     # Print error message.
-    echo "/dev/$disk_target failed imaged with $promox_ve_url_page Aborting..." && exit 1
+    echo "/dev/$disk_target failed imaged with $proxmox_ve_url_page Aborting..." && exit 1
 fi
 
 # Print success or exit for any errors.
 if [ $? -eq 0 ]
 then
-    echo "Successfully prepared Proxmox Version: $promox_ve_ver"
+    echo "Successfully prepared Proxmox Version: $proxmox_ve_ver"
 else
-    echo "Could not prepare Proxmox Version: $promox_ve_ver" >&2
+    echo "Could not prepare Proxmox Version: $proxmox_ve_ver" >&2
 fi
