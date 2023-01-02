97e88e899fbd33c3376ff50e4d4270ac9c2a86ef
codecio <matt@colecio.com>
2023-01-01 22:23:25 -0600

    feat(proxmox): Change base cloud image to ubuntu focal 20.04. The proxmox intergration works better with debian based distros it seems.

--- docs/proxmox/prep-template.sh
+++ docs/proxmox/prep-template.sh
@@ -1,35 +1,49 @@
 #!/usr/bin/env bash
 
-# variables
-fedora_cb_ver="37-1.7"
-fedora_cb_url_domain="https://download.fedoraproject.org"
-fedora_cb_url_path="/pub/fedora/linux/releases/37/Cloud/x86_64/images/"
-fedora_cb_url_base="$fedora_cb_url_domain$fedora_cb_url_path"
-fedora_cb_url_file="Fedora-Cloud-Base-$fedora_cb_ver.x86_64.raw.xz"
-fedora_cb_url_file_raw="Fedora-Cloud-Base-$fedora_cb_ver.x86_64.raw"
-fedora_cd_import_disk_target="data_zfs_sdb"
-fedora_vm_id=9000
+# Set script variables
+image_url_domain="https://cloud-images.ubuntu.com"
+image_url_path="/focal/current/"
+image_url_base="$image_url_domain$image_url_path"
+image_file="focal-server-cloudimg-amd64.img"
+image_import_disk_target="data_zfs_sdb"
+vm_id=9000
+template_name="ubuntu-20.04-cloudimg-template"
 
-# setup
+# Setup PVE packages required
 apt install cloud-init xz-utils libguestfs-tools -y
-wget --no-clobber $fedora_cb_url_base$fedora_cb_url_file
-unxz $fedora_cb_url_file
-virt-customize -a "$fedora_cb_url_file_raw" --install qemu-guest-agent
-qm create $fedora_vm_id \
+
+# Pull image from ubuntu repo
+wget --no-clobber $image_url_base$image_file
+
+# Install qemu-guest-agent to bake into img
+virt-customize -a $image_file --install qemu-guest-agent
+
+# Create VM
+qm create $vm_id \
 --memory 4096 --sockets 1 --cores 2 --vcpu 2 \
 --net0 virtio,bridge=vmbr0 \
 --hotplug network,disk,cpu,memory \
 --agent 1 \
---name fedora-cloud-base-37-cloudinit-template \
+--name $template_name \
 --ostype l26
-qm importdisk $fedora_vm_id $fedora_cb_url_file_raw $fedora_cd_import_disk_target
-qm set $fedora_vm_id --scsihw virtio-scsi-pci --virtio0 $fedora_cd_import_disk_target:vm-$fedora_vm_id-disk-0
-qm set $fedora_vm_id --ide2 $fedora_cd_import_disk_target:cloudinit
-qm set $fedora_vm_id --boot c --bootdisk virtio0
-qm set $fedora_vm_id --serial0 socket
-qm set $fedora_vm_id --numa 1
-qm template $fedora_vm_id
 
-# clean up
-rm -f Fedora-Cloud-Base-37-1.7.x86_64.raw.xz
-rm -f Fedora-Cloud-Base-37-1.7.x86_64.raw
+# Import Image
+qm importdisk $vm_id $image_file $image_import_disk_target
+
+# Bind image to a storage device
+qm set $vm_id --scsihw virtio-scsi-pci --virtio0 $image_import_disk_target:vm-$vm_id-disk-0
+
+# Add drive for cloud-init
+qm set $vm_id --ide2 $image_import_disk_target:cloudinit
+
+# Configure VM to boot from image
+qm set $vm_id --boot c --bootdisk virtio0
+
+# Add a serial console socket
+qm set $vm_id --serial0 socket
+
+# Convert the VM to a template
+qm template $vm_id
+
+# Clean up img file
+rm -f jammy-server-cloudimg-amd64.img
