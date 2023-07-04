72cf034c4b39fa93c1d541f3ce4401f53f5b98d2
codecio <matt@colecio.com>
2023-07-04 12:47:34 -0500

    fix: promox 8.0 permission issues

--- terraform/proxmox/providers.tf
+++ terraform/proxmox/providers.tf
@@ -3,6 +3,7 @@ provider "proxmox" {
   pm_api_token_secret = data.sops_file.pm_secrets.data["pm_api_token_secret"]
   pm_api_token_id     = data.sops_file.pm_secrets.data["pm_api_token_id"]
   pm_tls_insecure     = true
+  #pm_debug = true
 }
 
 provider "curl" {
